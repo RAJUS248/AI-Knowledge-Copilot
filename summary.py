dff3af7c45e4f2c186e3f120bf9c5ec17b1fc6b1
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-06-28 09:37:31 +0530

    Add files via upload

---
+++ summary.py
@@ -0,0 +1,29 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+def generate_summary(docs):
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        temperature=0.2
+    )
+
+    text = "\n".join(
+        doc.page_content
+        for doc in docs[:50]
+    )
+
+    prompt = f"""
+    Summarize this PDF.
+
+    Include:
+    - Main topics
+    - Key concepts
+    - Important takeaways
+
+    PDF Content:
+    {text}
+    """
+
+    response = llm.invoke(prompt)
+
+    return response.content
