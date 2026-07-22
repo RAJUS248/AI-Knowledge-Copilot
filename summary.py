7c50fb269ab0f8649b8a49b7b3d00db8f7771ab4
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-07-22 23:54:36 +0530

    Reformat generate_summary function in summary.py

--- summary.py
+++ summary.py
@@ -3,7 +3,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 def generate_summary(docs):
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-2.5-flash", 
         temperature=0.2  
     )  
  
